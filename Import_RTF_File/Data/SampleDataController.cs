bbf28410de4b7e764cd9f7e9f641b20585ac26cd
Saravanan G <saravanang@syncfusion.com>
2022-08-04 13:08:57 +0530

    Add files via upload

---
+++ Import_RTF_File/Data/SampleDataController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Microsoft.AspNetCore.Http.Features;
+using System.Text.RegularExpressions;
+
+namespace Import_RTF_File.Data
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleDataController : ControllerBase
+    {
+        private IWebHostEnvironment hostingEnv;
+
+        public SampleDataController(IWebHostEnvironment env)
+        {
+            this.hostingEnv = env;
+        }
+
+
+        [HttpPost]
+        [Route("Import")]
+        public string Import(IList<IFormFile> UploadFiles)
+        {
+            string HtmlString = string.Empty;
+            if (UploadFiles != null)
+            {
+                foreach (var file in UploadFiles)
+                {
+                    string filename = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    filename = hostingEnv.WebRootPath + "\\files" + $@"\{filename}";
+                    using (FileStream fs = System.IO.File.Create(filename))
+                    {
+                        file.CopyTo(fs);
+                        fs.Flush();
+                    }
+                    using (var mStream = new MemoryStream())
+                    {
+                        new WordDocument(file.OpenReadStream(), FormatType.Rtf).Save(mStream, FormatType.Html);
+                        mStream.Position = 0;
+                        HtmlString = new StreamReader(mStream).ReadToEnd();
+                    };
+                    HtmlString = ExtractBodyContent(HtmlString);
+                    var str = HtmlString.Replace("\r\n", "");
+                    Response.Headers.Add("rteValue", str);
+                }
+            }
+            return HtmlString;
+        }
+
+        public string ExtractBodyContent(string html)
+        {
+            if (html.Contains("<html") && html.Contains("<body"))
+            {
+                return html.Remove(0, html.IndexOf("<body>") + 6).Replace("</body></html>", "");
+            }
+            return html;
+        }
+
+        [HttpPost]
+        [Route("Save")]
+        public void Save(IList<IFormFile> UploadFiles)
+        {
+            try
+            {
+                foreach (var file in UploadFiles)
+                {
+                    if (UploadFiles != null)
+                    {
+                        string path = hostingEnv.ContentRootPath + "\\wwwroot\\Images";
+                        string filename = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                        if (!System.IO.Directory.Exists(path))
+                        {
+                            System.IO.Directory.CreateDirectory(path);
+                        }
+
+                        //To save the image in the sever side 
+                        filename = path + $@"\{filename}";
+
+                        if (!System.IO.File.Exists(filename))
+                        {
+                            using (FileStream fs = System.IO.File.Create(filename))
+                            {
+                                file.CopyTo(fs);
+                                fs.Flush();
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
